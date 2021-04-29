9aaf3457d121bf2332692fe6db44aa95cad669e8
bryantomoregie <unclebryant@gmail.com>
2021-04-28 23:42:21 -0500

    slowly, but surely

---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Navigation(){
+    return(
+        <nav>
+        <i className="fab fa-reddit"></i>
+        <h3>reddit</h3>
+        </nav>
+    )
+}
---
+++ src/Components/SignUpContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SignUpContainer(){
+
+    return(
+        <>
+        <div>
+        <h3>Sign Up</h3>
+        <p>By continuing, you agree to our <span>User Agreement</span> and <span>Privacy Policy</span>.</p>
+        </div>
+        </>
+    )
+}
